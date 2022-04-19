01914feeacfdc541a9a84c2fe8e221d03d0630f5
shanayR <shanay.rh@gmail.com>
2022-04-19 23:14:38 +0500

    conversion assignment

---
+++ importExport/conversion.js
@@ -0,0 +1,6 @@
+// celsius to farenheit conversion
+let convert = (celsius) => {
+    let farenheit = (celsius *9/5) + 32
+    return console.log(`${farenheit}F`);
+}
+export {convert}
--- importExport/import.js
+++ importExport/import.js
@@ -1,3 +1,5 @@
 import ab from "export.js";
+import convert from "conversion.js"
 
+console.log(convert(30));
 console.log(ab());
