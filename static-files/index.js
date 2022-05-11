317336168bb02549353a7528d7d79acbe67cafa0
maham <mahamtariq603@gmail.com>
2022-05-11 01:26:40 -0700

    ddd

--- static-files/index.js
+++ static-files/index.js
@@ -1,10 +1,10 @@
 import express from 'express';
 import path from 'path';
-import bodyParser from 'body-parser';
+
 const __dirname = path.resolve();
 const app = express();
 const port = 8080;
-const urlEncodeParse = bodyParser.urlencoded({extended:false});
+
 app.set('view engine', 'ejs');
 app.use('/static', express.static(path.join(__dirname,'assets')));
 // app.use(express.text())
@@ -13,13 +13,6 @@ app.use(express.urlencoded({extended:false}));
 app.get('/login',(req,res)=>{
     res.render('login')
 })
-
-// app.post('/postform',urlEncodeParse,(req,res)=>{
-//     if(req){
-//         console.log(req.body);
-//         res.send(`Welcome ${req.body.name}`);
-//     }
-// });
 
 app.post('/postform',  (req, res) =>{
     console.log(req.body.name)
