dbcb90ca5f9c1b7dc3d7c11b18c3c8ef0288407e
maham <mahamtariq603@gmail.com>
2022-05-23 00:23:47 -0700

    debuggg

---
+++ debug/index.js
@@ -0,0 +1,7 @@
+const array = [2000,2001,2002,2003]
+array.forEach(element => {
+    if (element == 2002){
+        console.log(true);
+    }    
+    else{console.log(false);}
+});
---
+++ exchange-rate-api/debug.js
@@ -0,0 +1,7 @@
+const year = [2000,2001,2002,2003]
+year.forEach(element => {
+    if (element == 2002){
+        console.log(true);
+    }    
+    else{console.log(false);}
+});
