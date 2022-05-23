eb1ceafe2c526ca10727cffcbaf143e8a91874a6
maham <mahamtariq603@gmail.com>
2022-05-23 01:27:18 -0700

    ffff

---
+++ classes/index.js
@@ -0,0 +1,27 @@
+class Person{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Teacher extends Person{
+    constructor(name,age,isTeacher){
+        super(name,age);
+        this.isTeacher = isTeacher;
+    }
+
+    teachers(){
+        if(this.isTeacher === true){
+            console.log(`${this.name} is a teacher`)
+        }
+        else{
+            console.log(`${this.name} is a student`)
+        }
+    }
+}
+const teacher1 = new Teacher("Ali",21,true)
+const teacher2 = new Teacher("Alia",12,false)
+teacher1.teachers()
+teacher2.teachers()
+
