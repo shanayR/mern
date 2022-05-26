2c76998bcf32c55c07dce5711dbc5fe19e00f9e2
maham <mahamtariq603@gmail.com>
2022-05-26 01:40:36 -0700

    sss

---
+++ chatApp/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import path from 'path';
+import http from 'http';
+import {Server} from 'socket.io';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+const __dirname = path.resolve();
+
+app.get('/', (req, res) => {
+//   res.send('<h1>Hello world</h1>');
+res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+    let usersConnected = socket.client.conn.server.clientsCount;
+    console.log( `users connected: ${usersConnected}`);
+    
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        console.log( `users connected: ${usersConnected} `);
+    });
+    
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        io.emit('chat message', msg);
+    });
+    io.emit('users connected', usersConnected);
+  });
+
+  
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
