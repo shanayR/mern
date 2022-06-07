5bb1b8cecea03c63c963b3b48b28e04136c420da
shanayR <shanay,rh@gmail.com>
2022-06-07 01:31:20 -0700

    neb

--- crud/index.js
+++ crud/index.js
@@ -1,33 +1,22 @@
+import dotenv from 'dotenv/config';
 import express from "express";
 import { MongoClient, ServerApiVersion } from 'mongodb';
+import { postForm, renderForm } from "./controller/controller.js";
 // import {router} from "./routes/routes.js";
 const app = express();
-const uri = "mongodb+srv://shanay:shanay786@cluster0.bljbdnt.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { 
+const client = new MongoClient(process.env.DATABASE_URI, { 
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     serverApi: ServerApiVersion.v1 });
 
 app.set('view engine', 'ejs');
-app.get('/',(req,res)=>{
-    res.render('add')
-})
+app.get('/',renderForm)
 app.use(express.urlencoded({extended:false}));
-app.post('/postdata',(req,res)=>{
-    client.connect( (err) => {
-        if (err) throw err;
-        const users = client.db("testdb").collection("users");
-        users.insertOne(req.body,(err,result) => {
-            if (err) throw err;
-            if(result.acknowledged){
-                console.log(result)
-            }else{console.log("error");}
-        })
-        res.redirect('/')
+app.post('/postdata',postForm)
+        
         // perform actions on the collection object
         // client.close();
-    });
-})
+    
 // app.use('/add', router);
 
 app.listen(8080)
